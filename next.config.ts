c1db69e6790edd43eeb67a77328682bb690dc060
Jimmy <nordstromjimmy@gmail.com>
2025-04-15 18:44:20 +0200

    commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Optional if your structure is under /src
-  experimental: {
-    appDir: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
