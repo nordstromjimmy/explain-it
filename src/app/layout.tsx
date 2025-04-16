616ecb9bfff72155445b0b04f25c46cfc3cf05ae
Jimmy <nordstromjimmy@gmail.com>
2025-04-16 18:14:18 +0200

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <head>
+        <script
+          defer
+          data-domain="explainit.io"
+          src="https://plausible.io/js/script.js"
+        ></script>
+      </head>
       <body className="min-h-screen bg-gray-100 text-gray-900 flex justify-center items-start p-6">
         {children}
       </body>
